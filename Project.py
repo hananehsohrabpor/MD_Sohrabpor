fed69431bf5da392130723609c8290d5f7aa76b0
MRS.Sohrabpor <hananesohrabpor82@gmail.com>
2023-12-23 12:42:05 -0800

    sakhte halgh while baray namayesh tasvir

--- Project.py
+++ Project.py
@@ -8,3 +8,11 @@ def get_frame_gray (cap):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray,(21,21),0)
     return frame,gray
+while True:
+    frame,gray = get_frame_gray(cap)
+    cv2.imshow('farme',frame)
+    cv2.imshow('gray blur',gray)
+    if cv2.waitKey(1) == 13:
+        break
+cap.release()
+cv2.destroyAllWindows()
