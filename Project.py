ae82d408aa3997502372d85dd1d56b6f9823a713
MRS.Sohrabpor <hananesohrabpor82@gmail.com>
2023-12-23 12:30:05 -0800

    sakht tabe baraye baz kardan dorbin

--- Project.py
+++ Project.py
@@ -1,2 +1,10 @@
 import cv2
 import numpy as np
+
+cap = cv2.VideoCapture(0)
+def get_frame_gray (cap):
+    ret, frame = cap.read()
+    frame = frame[:,130:550]
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray,(21,21),0)
+    return frame,gray
